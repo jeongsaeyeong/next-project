415b61a08e9aea7d91baebfe9cff8bc68eb5c0fb
jeongsaeyeong <ooooo0516@naver.com>
2023-11-13 10:08:08 +0900

    commit

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Next.js를 이용한 포트폴리오 사이트 만들기',
+  description: 'Nest.js를 이용한 사이트를 만들었습니다.',
+  keywords: ['포트폴리오', 'next.js', '리액트', 'protfolio']
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+      <body>{children}</body>
+    </html>
+  )
+}
